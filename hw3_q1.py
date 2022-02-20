4aa45d8f069a0feb40db158d6c626afff9232f03
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-02-20 13:34:14 -0500

    finished q1

---
+++ hw3_q1.py
@@ -0,0 +1,31 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW3 - Q1
+Date due: 2022-02-24
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+xp_points = float(input("Enter this user's current XP: "));
+invalid_xp = False;
+
+if(40.0 <= xp_points <= 50.0):
+    player_level = 5;
+elif(30.0 <= xp_points <= 39.9):
+    player_level = 4;
+elif(25.0 <= xp_points <= 29.9):
+    player_level = 3;
+elif(18.5 <= xp_points <= 24.9):
+    player_level = 2;
+elif(xp_points < 18.5):
+    player_level = 1;
+else:
+    invalid_xp = True;
+
+if(invalid_xp == False):
+    print("Level",player_level,"Player (XP:",xp_points,")");
+
+else:
+    print("ERROR: Please enter a valid XP value.");
