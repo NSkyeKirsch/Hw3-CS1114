991eab86fcf0a6acbe9163567b3d6ad9833a2e50
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-02-24 19:48:18 -0500

    finished q3

--- hw3_q2.py
+++ hw3_q2.py
@@ -14,15 +14,15 @@ Level = int(input("What is this Pokemon's Level? "));
 Speed =  int(input("What is this Pokemon's Speed? "));
 
 #r is random 0-255 and t is random from 0-255
-R = random.randint(0,255);
-T = Speed / 2; #threshold value
-if(R < T):
+RandNum = random.randint(0,255);
+Threshold = Speed / 2; #threshold value
+if(RandNum < Threshold):
     Critical_Hit = True;
 if(Critical_Hit == True):
-    M = round((2*Level+5) / (Level+5)); #number that the attack will be multiplied by
+    Multiplier = round((2*Level+5) / (Level+5)); #number that the attack will be multiplied by
 else:
-    M = 1;
-print("The Pokemon's move will be",str(M) + "x stronger!");
+    Multiplier = 1;
+print("The Pokemon's move will be",str(Multiplier) + "x stronger!");
 
 
 
---
+++ hw3_q4.py
@@ -0,0 +1,11 @@
+"""
+Author: Noa Kirschbaum
+Assignment / Part: HW3 - Q4
+Date due: 2022-02-24
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+a
