9e21705fee26ddcd2bc221c2bf1fe149f8c90cda
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-02-24 21:56:08 -0500

    finished q3

--- hw3_q4.py
+++ hw3_q4.py
@@ -7,5 +7,27 @@ collaborating with anyone else, in conformance with the
 NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
+import math;
 
-a
+a = float(input("a = "));
+b = float(input("b = "));
+c = float(input("c = "));
+
+#positive = 2 solutions, zero = 1 solution, negative = no real solution
+discriminant = (b**2.0) - (4.0*a*c);
+
+if(a == 0.0 and b == 0.0 and c == 0.0):
+    print("This equation has an infinite number of solutions.")
+elif(discriminant > 0.0):
+
+    solution_one = (-(b) + math.sqrt(discriminant))/(2.0*a);
+    solution_two = (-(b) - math.sqrt(discriminant))/(2.0*a);
+    print("This equation has two solutions: x =",solution_one,"and x =",solution_two);
+
+elif(discriminant == 0.0):
+
+    solution_one = (-(b) + math.sqrt(discriminant)) / (2.0 * a);
+    print("This equation has one solution: x =",solution_one);
+
+else:
+    print("No Real Solutions");
